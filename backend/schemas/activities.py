0f4e52458373e93b3f2965bf193346f3f6726fe4
payerl200131 <payerl200131@sr.htlweiz.at>
2025-01-20 11:11:21 +0100

    activities schema, crud, api

---
+++ backend/schemas/activities.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class Base(BaseModel):
+    activity_id: int
+    location: str
+    description: str
+    curriculum_reference: str
+    cost: int
+    transfer_cost: int
+    start_time: str
+    end_time: str
+    sga_approved: bool
+    state: str
+    create_time: str
+    last_update: str
+
+class Create(BaseModel):
+    location: str
+    description: str
+    curriculum_reference: str
+    cost: int
+    transfer_cost: int
+    start_time: datetime
+    end_time: datetime
+    sga_approved: Optional[bool] = False
+    state: Optional[str] = "REQUESTED"
+
+class Update(BaseModel):
+    location: Optional[str]
+    description: Optional[str]
+    curriculum_reference: Optional[str]
+    cost: Optional[int]
+    transfer_cost: Optional[int]
+    start_time: Optional[datetime]
+    end_time: Optional[datetime]
+    sga_approved: Optional[bool]
+    state: Optional[str]
+    last_update: Optional[datetime] = datetime.now()
