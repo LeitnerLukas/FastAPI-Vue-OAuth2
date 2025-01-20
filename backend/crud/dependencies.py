84f726f71461fc0adc700cf21b7fea32faaea2a1
LeitnerLukas <91020705+LeitnerLukas@users.noreply.github.com>
2025-01-20 10:35:46 +0100

    db models

--- backend/crud/dependencies.py
+++ backend/crud/dependencies.py
@@ -2,7 +2,13 @@ from typing import Generator
 
 from database.config import async_session
 from crud.user import UserCRUD
+from enum import Enum
 
+class AcceptState(Enum):
+    APPROVED = "approved"
+    SUPER_APPROVED = "super_approved"
+    REQUESTED = "requested"
+    DENIED = "denied"
 
 async def get_db() -> Generator:
     async with async_session() as session:
